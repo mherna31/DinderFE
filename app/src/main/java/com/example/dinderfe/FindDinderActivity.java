07e6171d5e594f6952295238b2b109ac38b8635e
maxH <mherna31@uncc.edu>
2020-11-03 17:42:53 -0500

    modified profile look and dinder button

--- app/src/main/java/com/example/dinderfe/FindDinderActivity.java
+++ app/src/main/java/com/example/dinderfe/FindDinderActivity.java
@@ -10,5 +10,6 @@ public class FindDinderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_dinder);
+        setTitle("Find Dinder");
     }
 }
--- app/src/main/java/com/example/dinderfe/MainActivity.java
+++ app/src/main/java/com/example/dinderfe/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("Profile");
+
+
 
         findViewById(R.id.buttonFindDinder).setOnClickListener(new View.OnClickListener() {
             @Override

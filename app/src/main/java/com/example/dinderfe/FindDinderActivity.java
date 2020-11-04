f9a7e0bf1f9a80c961a92839162dac7de5c582f9
maxH <mherna31@uncc.edu>
2020-11-03 22:58:07 -0500

    Added look to Find Dinder Activity xml

--- app/src/main/java/com/example/dinderfe/FindDinderActivity.java
+++ app/src/main/java/com/example/dinderfe/FindDinderActivity.java
@@ -3,13 +3,47 @@ package com.example.dinderfe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class FindDinderActivity extends AppCompatActivity {
+public class FindDinderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_dinder);
         setTitle("Find Dinder");
+
+        Spinner cuisine = findViewById(R.id.spinnerCuisine);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.cuisines, R.layout.spinner_layout);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        cuisine.setAdapter(adapter);
+        cuisine.setOnItemSelectedListener(this);
+
+        Button surprise = findViewById(R.id.buttonSurpriseMe);
+        Button next = findViewById(R.id.buttonNext);
+        next.setBackgroundColor(getResources().getColor(R.color.red_theme));
+        surprise.setBackgroundColor(getResources().getColor(R.color.red_theme));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Log.d("demo", "onItemSelected: "+text);
+        if (text.endsWith("Select Cuisine") || text.matches( "------------")){
+            Toast.makeText(parent.getContext(),"Make a selection",Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }

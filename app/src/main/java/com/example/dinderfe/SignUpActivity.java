ee6a8a83c27dfb6607cc14c9dc5c80811e104108
Max Hernandez <mherna31@uncc.edu>
2020-12-14 19:36:12 -0500

    -Added signup activity

--- app/src/main/java/com/example/dinderfe/SignUpActivity.java
+++ app/src/main/java/com/example/dinderfe/SignUpActivity.java
@@ -7,26 +7,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LogInActivity extends AppCompatActivity {
-
-    Button login, signup;
-
+public class SignUpActivity extends AppCompatActivity {
+    Button create, back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_sign_up);
 
-        login = findViewById(R.id.buttonLogin);
-        signup = findViewById(R.id.buttonSignUp);
+        create = findViewById(R.id.buttonConfirm);
+        back = findViewById(R.id.buttonBack);
 
-        signup.setOnClickListener(new View.OnClickListener() {
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this,SignUpActivity.class);
+                Intent intent = new Intent(getApplicationContext(),LogInActivity.class);
                 startActivity(intent);
             }
         });
-
-
     }
 }

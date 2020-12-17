c7a4ad7148aed453b3b53eaa27b44e08c7ab873f
maxH <mherna31@uncc.edu>
2020-12-17 06:45:17 -0500

    -Create Profile class

---
+++ app/src/main/java/com/example/dinderfe/Profile.java
@@ -0,0 +1,56 @@
+package com.example.dinderfe;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Profile {
+    String first, last, avatar;
+    int gender, diet, interest;
+
+    public Profile(JSONObject body) throws JSONException{
+
+        this.first = body.getString("firstname");
+        this.last = body.getString("lastname");
+        this.avatar = null;
+        this.gender = body.getInt("gender");
+        this.diet = body.getInt("dietaryRestrictions");
+        this.interest = body.getInt("interest");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "first='" + first + '\'' +
+                ", last='" + last + '\'' +
+                ", avatar='" + avatar + '\'' +
+                ", gender=" + gender +
+                ", diet=" + diet +
+                ", interest=" + interest +
+                '}';
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public int getDiet() {
+        return diet;
+    }
+
+    public int getInterest() {
+        return interest;
+    }
+}
